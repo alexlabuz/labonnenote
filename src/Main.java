9d59c23732fb15d3afdb4898d1375aa8f75c1fda
alexlabuz <alex.labuz@live.fr>
2020-03-12 19:48:44 +0100

    initiation de JavaFX

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        System.out.print("LA BONNE NOTE");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("La Bonne Note (beta)");
+        Group root = new Group();
+        // Création de la fenêtre
+        primaryStage.setScene(new Scene(root, 300, 275, Color.WHITE));
+        primaryStage.show();
+
+    }
+}
