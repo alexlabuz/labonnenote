47608f54c11a5684aff1995cde346623492b7f81
alexlabuz <alex.labuz@live.fr>
2020-03-30 19:30:40 +0200

    Debut du main et ajout des matières

---
+++ src/donnee/saisirNombre.java
@@ -0,0 +1,46 @@
+/*
+Mettre la ligne suivante :
+
+import static com.company.saisirNombre.*;
+ */
+package outil;
+
+import java.util.Scanner;
+
+public class saisirNombre {
+
+    static int saisirEntier(int min, int max){
+        Scanner input = new Scanner(System.in);
+        try{
+            int nombre = input.nextInt();
+            if((nombre >= min && nombre <= max) || (min == 0 && max == 0)){
+                return nombre;
+            }else{
+                System.out.print("Erreur de saisie, réessayez : ");
+                return saisirEntier(min, max);
+            }
+        }
+        catch (Exception e){
+            System.out.print("Erreur de saisie, réessayez : ");
+            return saisirEntier(min, max);
+        }
+    }
+
+    static double saisirReel(double min, double max){
+        Scanner input = new Scanner(System.in);
+        try{
+            double nombre = input.nextDouble();
+            if((nombre >= min && nombre <= max) || (min == 0 && max == 0)){
+                return nombre;
+            }else{
+                System.out.print("Erreur de saisie, réessayez : ");
+                return saisirReel(min, max);
+            }
+        }
+        catch (Exception e){
+            System.out.print("Erreur de saisie, réessayez : ");
+            return saisirReel(min, max);
+        }
+    }
+
+}
